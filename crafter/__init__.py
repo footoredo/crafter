82cfcefa96ec074ccf3957cafaba3b1b4f822d8e
footoredo <footoredo@gmail.com>
2021-09-26 11:47:56 -0400

    partial achievement

--- crafter/__init__.py
+++ crafter/__init__.py
@@ -2,16 +2,22 @@ from .env import Env
 from .recorder import Recorder
 
 try:
-  import gym
-  gym.register(
-      id='CrafterReward-v1',
-      entry_point='crafter:Env',
-      max_episode_steps=10000,
-      kwargs={'reward': True})
-  gym.register(
-      id='CrafterNoReward-v1',
-      entry_point='crafter:Env',
-      max_episode_steps=10000,
-      kwargs={'reward': False})
+    import gym
+
+    gym.register(
+        id='CrafterReward-v1',
+        entry_point='crafter:Env',
+        max_episode_steps=10000,
+        kwargs={'reward': True})
+    gym.register(
+        id='CrafterNoReward-v1',
+        entry_point='crafter:Env',
+        max_episode_steps=10000,
+        kwargs={'reward': False})
+    gym.register(
+        id='CrafterPartialCollectIron-v1',
+        entry_point='crafter:Env',
+        max_episode_steps=10000,
+        kwargs={'reward': True, 'partial_achievements': 'collect_iron'})
 except ImportError:
-  pass
+    pass
