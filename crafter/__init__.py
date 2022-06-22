4be3d6d69d995ff991e9969603062592231f3916
footoredo <footoredo@gmail.com>
2022-06-22 12:27:43 -0400

    update

--- crafter/__init__.py
+++ crafter/__init__.py
@@ -5,6 +5,19 @@ try:
     import gym
 
     gym.register(
+        id='CrafterNew-v1',
+        entry_point='crafter:Env',
+        max_episode_steps=10000,
+        kwargs={
+            'reward': True, 
+            'size': (84, 84), 
+            'render_centering': False, 
+            'health_reward_coef': 0.0,
+            'immortal': True,
+            'idle_death': 100
+        })
+
+    gym.register(
         id='CrafterReward-v1',
         entry_point='crafter:Env',
         max_episode_steps=10000,
